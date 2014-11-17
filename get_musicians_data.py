c0e1bda95e6312563f56475cbabcb30a7dad4ac7
Sean Lee <iseansay@gmail.com>
2014-11-17 13:00:56 +0800

    store name instead of slug

--- get_musicians_data.py
+++ get_musicians_data.py
@@ -10,17 +10,16 @@ musician_page_1 = wikipedia.page("%s: A to M" % BASE_PAGE_NAME)
 musician_page_2 = wikipedia.page("%s: N to Z" % BASE_PAGE_NAME)
 
 
-def get_all_musicians_slug():
-    slugs = []
+def get_all_musician_names():
+    names = []
 
     for page in [musician_page_1, musician_page_2]:
-        slugs += get_musicians_slug_by_page(page)
+        names += get_musician_names_by_page(page)
 
-    return slugs
+    return names
 
 
-
-def get_musicians_slug_by_page(page):
+def get_musician_names_by_page(page):
     musicians = []
 
     soup = BeautifulSoup(page.html())
@@ -31,10 +30,10 @@ def get_musicians_slug_by_page(page):
 
     for musician in all_musicians:
         m_attrs = musician.attrs
+        name = m_attrs['title']
         link = m_attrs['href']
         if link.startswith('/wiki/'):
-            slug = link.split('/')[-1]
-            musicians.append(slug)
+            musicians.append(name)
         else:
             print 'not exist'
     return musicians
@@ -42,3 +41,8 @@ def get_musicians_slug_by_page(page):
 
 def get_musician_data_by_name(name):
     page = wikipedia.page(name)
+
+
+if __name__ == '__main__':
+    all_musician_names = get_all_musician_names()
+    print "all musicians count %s" % len(all_musician_names)
